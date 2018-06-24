c0abfa349ed0e53f56c6160be4e8286c626e7cef
RomeoKilo125 <romeokilo125@gmail.com>
2018-06-24 13:23:16 -0400

    updated schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,9 @@
-CREATE DATABASE IF NOT EXISTS burgers_db;
-
 USE burgers_db;
 
 CREATE TABLE IF NOT EXISTS burgers
 (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   burger_name VARCHAR(45),
+  ceratedAt TIMESTAMP NOT NULL,
   devoured BOOLEAN DEFAULT false
 );
